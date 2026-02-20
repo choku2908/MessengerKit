da4c4ae37fb69d2d8555eaa63b10dec6f78d9b68
Hoon Sung <hoon.sung@sendbird.com>
2026-02-20 20:45:00 +0900

    Add XIB resources to Package.swift and use Bundle.module for SPM

--- MessengerKit/MessengerKit.swift
+++ MessengerKit/MessengerKit.swift
@@ -9,29 +9,33 @@
 import Foundation
 
 public final class MessengerKit {
-    
+
     /// The bundle that contains nibs and assets.
     internal static var bundle: Bundle? {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
         let sourceBundle = Bundle(for: MessengerKit.self)
-        
+
         if let url = sourceBundle.url(forResource: "MessengerKit", withExtension: "bundle"), let bundle = Bundle(url: url) {
             return bundle
         }
-        
+
         return sourceBundle
+        #endif
     }
-    
+
     private init() { }
-    
+
     /// Built-in styles to be used by `MSGMessengerViewController`
     public struct Styles {
-        
+
         public static let travamigos = MSGTravamigosStyle()
-        
+
         public static let iMessage = MSGIMessageStyle()
-        
+
         private init() { }
-        
+
     }
-    
+
 }
--- Package.swift
+++ Package.swift
@@ -14,7 +14,25 @@ let package = Package(
             name: "MessengerKit",
             path: "MessengerKit",
             resources: [
-                .process("Assets.xcassets")
+                .process("Assets.xcassets"),
+                .process("Messenger View/MSGMessengerView.xib"),
+                .process("Input/Styles/iMessage/MSGImessageInputView.xib"),
+                .process("Collection View/Cells/Emoji/MSGIncomingEmojiCollectionViewCell.xib"),
+                .process("Collection View/Cells/Emoji/MSGOutgoingEmojiCollectionViewCell.xib"),
+                .process("Collection View/Cells/Media/MSGIncomingImageCollectionViewCell.xib"),
+                .process("Collection View/Cells/Media/MSGOutgoingImageCollectionViewCell.xib"),
+                .process("Collection View/Cells/Text/iMessage/MSGIncomingTailCollectionViewCell.xib"),
+                .process("Collection View/Cells/Text/iMessage/MSGOutgoingTailCollectionViewCell.xib"),
+                .process("Collection View/Cells/Text/Travamigos/MSGIncomingTravCollectionViewCell.xib"),
+                .process("Collection View/Cells/Text/Travamigos/MSGOutgoingTravCollectionViewCell.xib"),
+                .process("Collection View/Header Footers/iMessage/MSGImessageIncomingFooterView.xib"),
+                .process("Collection View/Header Footers/iMessage/MSGImessageIncomingHeaderView.xib"),
+                .process("Collection View/Header Footers/iMessage/MSGImessageOutgoingFooterView.xib"),
+                .process("Collection View/Header Footers/iMessage/MSGImessageOutgoingHeaderView.xib"),
+                .process("Collection View/Header Footers/Travamigos/MSGTravIncomingCollectionViewSectionFooter.xib"),
+                .process("Collection View/Header Footers/Travamigos/MSGTravIncomingCollectionViewSectionHeader.xib"),
+                .process("Collection View/Header Footers/Travamigos/MSGTravOutgoingCollectionViewSectionFooter.xib"),
+                .process("Collection View/Header Footers/Travamigos/MSGTravOutgoingCollectionViewSectionHeader.xib"),
             ]
         )
     ]
